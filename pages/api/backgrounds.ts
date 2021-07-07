3d9690b8fa805e0a7b7af09f810cffabcf8d1bbb
Simon Huang <me@simonh.io>
2021-07-07 01:45:39 -0700

    Further split background loading

--- pages/api/backgrounds.ts
+++ pages/api/backgrounds.ts
@@ -10,7 +10,7 @@ const [nightfromday, LateSunset] = [
   makeLookupTable('/LateSunset.3DL'),
 ];
 
-const backgrounds:Map<string, Record<string, Buffer>> = new Map();
+const backgrounds:Record<string, Record<string, Buffer>> = {};
 
 const getBackgrounds = async (
   req: NextApiRequest, res: NextApiResponse,
@@ -18,31 +18,53 @@ const getBackgrounds = async (
   const { url, type } = req.query;
   const urlString = (url || '').toString();
   const typeString = (type || '').toString();
-  const backgroundObject = backgrounds.get(urlString);
-  if (backgroundObject && backgroundObject[typeString]) {
+  if (backgrounds[urlString] && backgrounds[urlString][typeString]) {
     res.status(200)
       .setHeader('Content-Type', 'image/png')
-      .setHeader('Content-Length', backgroundObject[type.toString()].length)
-      .send(backgroundObject[type.toString()]);
+      .setHeader('Content-Length', backgrounds[urlString][typeString].length)
+      .send(backgrounds[urlString][typeString]);
   } else {
     const [nightFromDay, lateSunset] = await Promise.all([nightfromday, LateSunset]);
     const png = fs.createReadStream(path.join(process.cwd(), 'public', urlString)).pipe(new PNG());
     if (png) {
       png.on('parsed', async () => {
-        const resultHolder:Record<string, Buffer> = {
-          original: PNG.sync.write(png),
-        };
-        const sunset = colorLookup(lateSunset, png.data);
-        const night = colorLookup(nightFromDay, png.data);
-        png.data = sunset;
-        resultHolder.sunset = PNG.sync.write(png);
-        png.data = night;
-        resultHolder.night = PNG.sync.write(png);
-        backgrounds.set(urlString, resultHolder);
-        res.status(200)
-          .setHeader('Content-Type', 'image/png')
-          .setHeader('Content-Length', resultHolder[typeString].length)
-          .send(resultHolder[typeString]);
+        if (typeString === 'original') {
+          const result = PNG.sync.write(png);
+          res.status(200)
+            .setHeader('Content-Type', 'image/png')
+            .setHeader('Content-Length', result.length)
+            .send(result);
+          backgrounds[urlString] = {
+            ...backgrounds[urlString],
+            original: result,
+          };
+        } else if (typeString === 'sunset') {
+          const sunset = colorLookup(lateSunset, png.data);
+          png.data = sunset;
+          const result = PNG.sync.write(png);
+          res.status(200)
+            .setHeader('Content-Type', 'image/png')
+            .setHeader('Content-Length', result.length)
+            .send(result);
+          backgrounds[urlString] = {
+            ...backgrounds[urlString],
+            sunset: result,
+          };
+        } else if (typeString === 'night') {
+          const night = colorLookup(nightFromDay, png.data);
+          png.data = night;
+          const result = PNG.sync.write(png);
+          res.status(200)
+            .setHeader('Content-Type', 'image/png')
+            .setHeader('Content-Length', result.length)
+            .send(result);
+          backgrounds[urlString] = {
+            ...backgrounds[urlString],
+            sunset: result,
+          };
+        } else {
+          res.status(404).send('404');
+        }
       });
     } else {
       res.status(404).send('404');
