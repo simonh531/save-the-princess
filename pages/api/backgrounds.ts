f5e886db4a74a533a5c45b2c735fa518b028cbd7
Simon Huang <me@simonh.io>
2021-07-07 01:26:25 -0700

    Made backgrounds API not load all at once

--- pages/api/backgrounds.ts
+++ pages/api/backgrounds.ts
@@ -3,56 +3,50 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
 import { PNG } from 'pngjs';
-import locations from '../../locations';
 import colorLookup, { makeLookupTable } from '../../utils/colorLookup';
 
-async function loadBackgrounds():Promise<Map<string, Record<string, Buffer>>> {
-  const holder = new Map();
-  const [nightfromday, LateSunset] = await Promise.all([
-    makeLookupTable('/nightfromday.CUBE'),
-    makeLookupTable('/LateSunset.3DL'),
-  ]);
-  const uniqueBackgrounds = new Set(
-    Object.values(locations).map((location) => location.background),
-  );
-  await Promise.all(Array.from(uniqueBackgrounds).map((url) => {
-    const png = fs.createReadStream(path.join(process.cwd(), 'public', url)).pipe(new PNG());
-    return new Promise<void>((resolve) => {
-      png.on('parsed', async () => {
-        const resultHolder:Record<string, Buffer> = {
-          original: PNG.sync.write(png),
-        };
-        const sunset = colorLookup(LateSunset, png.data);
-        const night = colorLookup(nightfromday, png.data);
-        png.data = sunset;
-        resultHolder.sunset = PNG.sync.write(png);
-        png.data = night;
-        resultHolder.night = PNG.sync.write(png);
-        holder.set(url, resultHolder);
-        resolve();
-      });
-    });
-  }));
+const [nightfromday, LateSunset] = [
+  makeLookupTable('/nightfromday.CUBE'),
+  makeLookupTable('/LateSunset.3DL'),
+];
 
-  return holder;
-}
-
-const backgroundsPromise = loadBackgrounds();
+const backgrounds:Map<string, Record<string, Buffer>> = new Map();
 
 const getBackgrounds = async (
   req: NextApiRequest, res: NextApiResponse,
 ):Promise<void> => {
-  const backgrounds = await backgroundsPromise;
   const { url, type } = req.query;
-
-  const backgroundObject = backgrounds.get((url || '').toString());
-  if (backgroundObject && backgroundObject[(type || '').toString()]) {
+  const urlString = (url || '').toString();
+  const typeString = (type || '').toString();
+  const backgroundObject = backgrounds.get(urlString);
+  if (backgroundObject && backgroundObject[typeString]) {
     res.status(200)
       .setHeader('Content-Type', 'image/png')
       .setHeader('Content-Length', backgroundObject[type.toString()].length)
       .send(backgroundObject[type.toString()]);
   } else {
-    res.status(404).send('404');
+    const [nightFromDay, lateSunset] = await Promise.all([nightfromday, LateSunset]);
+    const png = fs.createReadStream(path.join(process.cwd(), 'public', urlString)).pipe(new PNG());
+    if (png) {
+      png.on('parsed', async () => {
+        const resultHolder:Record<string, Buffer> = {
+          original: PNG.sync.write(png),
+        };
+        const sunset = colorLookup(lateSunset, png.data);
+        const night = colorLookup(nightFromDay, png.data);
+        png.data = sunset;
+        resultHolder.sunset = PNG.sync.write(png);
+        png.data = night;
+        resultHolder.night = PNG.sync.write(png);
+        backgrounds.set(urlString, resultHolder);
+        res.status(200)
+          .setHeader('Content-Type', 'image/png')
+          .setHeader('Content-Length', resultHolder[typeString].length)
+          .send(resultHolder[typeString]);
+      });
+    } else {
+      res.status(404).send('404');
+    }
   }
 };
 
